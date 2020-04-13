0f938f7b0bbf69ba066408befd4d96871c796892
NehorayFromIsrael <neosh8132@gmial.com>
2020-04-13 20:18:31 +0300

    fixed untracked files

---
+++ Test.py
@@ -0,0 +1,12 @@
+import Program.Get_Other_Functions as Get_Other_Functions
+from threading import *
+from queue import Queue
+import time
+from Program.Send import Send as Send
+
+
+token = ["771941522:AAGpG71f6B50Kch2QANTihxyKwdImWWmHB8"]
+id = "42155571"
+
+
+Send.Forword(token,id,id,1555)
